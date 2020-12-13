ecfcd84594b75203cfcddfb8940394cd1c272b7c
Teddy Weisman <tjweisman@gmail.com>
2020-12-13 16:33:51 -0600

    added license, readme and prepped setup for initial distribution

---
+++ README.md
@@ -0,0 +1,13 @@
+geometry_tools package
+----------------------
+
+This package consists of some basic tools for working with hyperbolic
+geometry, projective geometry, and group representations. Everything is done
+numerically, and a lot of the code is really sensitive to round-off error, so
+don't expect to use this to verify anything deep.
+
+Mostly, I use these tools to draw nice-ish pictures with matplotlib (and
+that's probably all they're good for).
+
+If you have questions or have found bugs, email me at
+[tjweisman@gmail.com](mailto:tjweisman@gmail.com).
--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="GeometryTools",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
 
     install_requires=[
         "numpy",
-        "matplotlib"
+        "matplotlib",
+        "scipy"
     ],
 
     author="Teddy Weisman",
     author_email="tjweisman@gmail.com",
-    description="""Some basic tools for working with projective space and the
-    projective plane""",
+    description="""Some basic tools for working with projective space, hyperbolic
+    geometry, and group representations"""
 )
